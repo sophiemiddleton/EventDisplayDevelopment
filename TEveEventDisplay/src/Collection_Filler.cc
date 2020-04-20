d422e13d7442a1258a0b21dd76c1ee89d62a8243
sophieMu2e <sophie@fnal.gov>
2020-04-19 20:28:34 -0500

    indented collection filled

--- TEveEventDisplay/src/Collection_Filler.cc
+++ TEveEventDisplay/src/Collection_Filler.cc
@@ -13,56 +13,57 @@ namespace mu2e{
 
   Collection_Filler::Collection_Filler(const Config& conf) :
     chTag_(conf.chTag()),
-	  gensTag_(conf.gensTag()),
-	  strawdigiTag_(conf.strawdigiTag()),
-	  crvcoinTag_(conf.crvdigiTag()),
+    gensTag_(conf.gensTag()),
+    strawdigiTag_(conf.strawdigiTag()),
+    crvcoinTag_(conf.crvdigiTag()),
     cluTag_(conf.cluTag()),
     cryHitTag_(conf.cryHitTag()),
     hseedTag_(conf.hseedTag()),
     kalseedTag_(conf.kalseedTag()),
-	  addHits_(conf.addHits()),
-	  addTracks_(conf.addTracks()),
-	  addClusters_(conf.addClusters()),
-	  addCrvHits_(conf.addCrvHits()),
-	  addCosmicSeedFit_(conf.addCosmicSeedFit()),
+    addHits_(conf.addHits()),
+    addTracks_(conf.addTracks()),
+    addClusters_(conf.addClusters()),
+    addCrvHits_(conf.addCrvHits()),
+    addCosmicSeedFit_(conf.addCosmicSeedFit()),
     addCRV_(conf.addCRV()),
-	  isCosmic_(conf.isCosmic()),
+    isCosmic_(conf.isCosmic()),
     MCOnly_(conf.MCOnly()),
     FillAll_(conf.FillAll())
-    {}
+  {}
 
+//Note RecoDataProducts is a list inside the header of this class (in dictclasses) add any additional classes to that list//
     
-      void Collection_Filler::FillRecoCollection(const art::Event& evt, Data_Collections &data, RecoDataProductName CollectionName){
+  void Collection_Filler::FillRecoCollection(const art::Event& evt, Data_Collections &data, RecoDataProductName CollectionName){
 
-        if(FillAll_ or(addHits_ and CollectionName==ComboHits)){ 
-          auto chH = evt.getValidHandle<mu2e::ComboHitCollection>(chTag_);
-          data.chcol = chH.product();
-        }
-        if(FillAll_ or CollectionName == CaloCrystalHits){
-          auto chH = evt.getValidHandle<mu2e::CaloCrystalHitCollection>(cryHitTag_);
-          data.cryHitcol = chH.product();
-        }
-        if(FillAll_ or (addClusters_ and CollectionName==CaloClusters)){
-          auto chH = evt.getValidHandle<mu2e::CaloClusterCollection>(cluTag_);
-          data.clustercol = chH.product();
-        }
-        if(FillAll_ or (isCosmic_ and addTracks_ and CollectionName==CosmicTracks)){
-          auto chH = evt.getValidHandle<mu2e::CosmicTrackSeedCollection>(cosmicTag_);
-          data.cosmiccol = chH.product();
-        }
-        if(FillAll_ or (!isCosmic_ and addTracks_ and CollectionName==HelixSeeds)){
-          auto chH = evt.getValidHandle<mu2e::HelixSeedCollection>(hseedTag_);
-          data.hseedcol = chH.product();
-        }
-        if(FillAll_ or (!isCosmic_ and addTracks_ and CollectionName==KalSeeds)){
-          auto chH = evt.getValidHandle<mu2e::KalSeedCollection>(kalseedTag_);
-          data.kalseedcol = chH.product();
-        }
-       if(FillAll_ or (addCRV_ and CollectionName==CRVCoincidences)){
-          auto chH = evt.getValidHandle<mu2e::CrvCoincidenceClusterCollection>(crvcoinTag_);
-          data.crvcoincol = chH.product();
-        }
-    }
+  if(FillAll_ or(addHits_ and CollectionName==ComboHits)){ 
+    auto chH = evt.getValidHandle<mu2e::ComboHitCollection>(chTag_);
+    data.chcol = chH.product();
+  }
+  if(FillAll_ or CollectionName == CaloCrystalHits){
+    auto chH = evt.getValidHandle<mu2e::CaloCrystalHitCollection>(cryHitTag_);
+    data.cryHitcol = chH.product();
+  }
+  if(FillAll_ or (addClusters_ and CollectionName==CaloClusters)){
+    auto chH = evt.getValidHandle<mu2e::CaloClusterCollection>(cluTag_);
+    data.clustercol = chH.product();
+  }
+  if(FillAll_ or (isCosmic_ and addTracks_ and CollectionName==CosmicTracks)){
+    auto chH = evt.getValidHandle<mu2e::CosmicTrackSeedCollection>(cosmicTag_);
+    data.cosmiccol = chH.product();
+  }
+  if(FillAll_ or (!isCosmic_ and addTracks_ and CollectionName==HelixSeeds)){
+    auto chH = evt.getValidHandle<mu2e::HelixSeedCollection>(hseedTag_);
+    data.hseedcol = chH.product();
+  }
+  if(FillAll_ or (!isCosmic_ and addTracks_ and CollectionName==KalSeeds)){
+    auto chH = evt.getValidHandle<mu2e::KalSeedCollection>(kalseedTag_);
+    data.kalseedcol = chH.product();
+  }
+  if(FillAll_ or (addCRV_ and CollectionName==CRVCoincidences)){
+    auto chH = evt.getValidHandle<mu2e::CrvCoincidenceClusterCollection>(crvcoinTag_);
+    data.crvcoincol = chH.product();
+  }
+  }
 
 /*
 //TODO- below is some thoughts - need to discuss - might need moving to the DataCollection
